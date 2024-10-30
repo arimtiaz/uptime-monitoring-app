5317850be203dfe991c5ae3b9c809e2feba2f6f9
Abadur Rahman <abadur391@gmail.com>
2024-10-30 13:05:39 +0600

    Delete method set

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -104,7 +104,69 @@ handler._user.post = (requestProperties, callback) => {
   }
 };
 // put Method
-handler._user.put = (requestProperties, callback) => {};
+handler._user.put = (requestProperties, callback) => {
+  const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
+
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName
+      : false;
+
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+
+  if(phone){
+    if(firstName || lastName || password){
+      data.read('users', phone, (err, uData) =>{
+        const userData = {...parseJSON(uData)}
+        if(!err && userData){
+          if(firstName){
+            userData.firstName = firstName;
+          }
+          if(lastName){
+            userData.lastName = lastName;
+          }
+          if(password){
+            userData.password = hash(password);
+          }
+          data.update('users', phone, userData, (err) =>{
+            if(!err){
+              callback(200, {
+                message: 'User updated successfully'
+              })
+            }else{
+              callback(500, {
+                error:'There was problem in the server side'
+              })
+            }
+          })
+        }
+      })
+    }else{
+      callback(400, {
+        error: 'Invalid phone number'
+      })
+    }
+  }else{
+    callback(400, {
+      error: 'You have a problem in your request'
+    })
+  }
+};
 // delete Method
 handler._user.delete = (requestProperties, callback) => {};
 
